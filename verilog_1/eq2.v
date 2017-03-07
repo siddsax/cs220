1a595895acf9cb76e7cbb1ffa9af0ff3024e51ff
shobhitr97 <shobhitr97@gmail.com>
2017-03-08 01:43:31 +0530

    Add files via upload

--- verilog_1/eq2.v
+++ verilog_1/eq2.v
@@ -1,14 +1,14 @@
-module SINBIT(A1, B1, Equal1);
+module eq1(A1, B1, Equal1);
 	input A1,B1;
 	output Equal1;
 	assign Equal1 = (A1==B1);
 endmodule
 
-module TWOBIT(A2,B2,Equal2);
+module eq2(A2,B2,Equal2);
 	input [1:0] A2,B2;
 	output Equal2;
-	reg eq1, eq2;
-	SINBIT forFirstBit( .A1(A2[1]), .B1(B2[1]), .Equal1(eq2));
-	SINBIT forSecondBit( .A1(A2[0]), .B1(B2[0]), .Equal1(eq1));
-	assign Equal2 = (eq1 & eq2);
+	reg e1, e2;
+	eq1 forFirstBit( .A1(A2[1]), .B1(B2[1]), .Equal1(e2));
+	eq1 forSecondBit( .A1(A2[0]), .B1(B2[0]), .Equal1(e1));
+	assign Equal2 = (e1 & e2);
 endmodule
